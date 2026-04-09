a6b4c58ac00b46892a818250515c7a2348943b4d
vietle9204 <vietle9204@gmail.com>
2026-04-09 11:59:21 +0700

    update

--- src/my_robot_kinematic/my_robot_kinematic/state_estimate_UKF2.py
+++ src/my_robot_kinematic/my_robot_kinematic/state_estimate_UKF2.py
@@ -54,7 +54,7 @@ class state_estimate(Node):
         self.L = self.n_x + self.n_w
 
         # define sigma point parameter
-        self.alpha = 0.25
+        self.alpha = 0.5
         self.beta = 2
         self.kappa = 0
         self.lam = self.alpha**2 * (self.L + self.kappa) - self.L
