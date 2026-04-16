49849916156b9ac829c5eda2476fedb2aa8dfbc2
vietle9204 <vietle9204@gmail.com>
2026-04-16 12:17:39 +0700

    update

--- src/ekf_slam/ekf_slam/ukf_slam.py
+++ src/ekf_slam/ekf_slam/ukf_slam.py
@@ -247,9 +247,9 @@ class UKFSLAM(Node):
         # Q_incremental: 
         dist = math.sqrt(dx_robot**2 + dy_robot**2)
         Q_robot = np.diag([
-            0.0049 * dist + 1e-12,        # Nhiễu x
-            0.0049 * dist + 1e-12,        # Nhiễu y
-            0.0049 * math.fabs(dtheta**2) + 1e-12   # Nhiễu theta
+            0.0025 * dist + 1e-12,        # Nhiễu x
+            0.0025 * dist + 1e-12,        # Nhiễu y
+            0.0025 * math.fabs(dtheta**2) + 1e-12   # Nhiễu theta
         ])
  
         self.predict((dx_robot, dy_robot, dtheta), Q_robot)
