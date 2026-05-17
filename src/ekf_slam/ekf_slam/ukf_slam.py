736c35c2911796b90419ab146bf7dd5488f1074a
vietle9204 <vietle9204@gmail.com>
2026-05-17 23:58:48 +0700

    .

--- src/ekf_slam/ekf_slam/ukf_slam.py
+++ src/ekf_slam/ekf_slam/ukf_slam.py
@@ -1414,7 +1414,7 @@ class UKFSLAM(Node):
         # self.processing = False
         # timer
         self.timer = self.create_timer(
-            0.015,
+            0.005,
             self.process,
             # callback_group=self.cb_group
         )
